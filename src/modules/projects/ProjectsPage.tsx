f5b7053eec2a1646e9768933bebac3e22acd16c3
TEC037 <105821219+TEC037@users.noreply.github.com>
2026-09-13 02:42:08 -0500

    feat(forja): puente acustico reactivo y sincronizacion de almacenamiento en el taller

--- src/modules/projects/ProjectsPage.tsx
+++ src/modules/projects/ProjectsPage.tsx
@@ -6,11 +6,46 @@
  * del manifiesto (Fase 5), sin acoplar este código al scoring.
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import ForjaApp from './forja/src/App';
 import './forja/src/index.css';
+import { soundEngine } from '../../core/sound';
 
 export const ProjectsPage: React.FC = () => {
+  useEffect(() => {
+    const handleNextAction = () => {
+      soundEngine.playComplete();
+      window.dispatchEvent(new Event('storage_projects_updated'));
+    };
+
+    const handleProjectCompleted = () => {
+      soundEngine.playMilestone();
+      window.dispatchEvent(new Event('storage_projects_updated'));
+    };
+
+    const handleProjectAdvanced = () => {
+      soundEngine.playTap();
+      window.dispatchEvent(new Event('storage_projects_updated'));
+    };
+
+    const handleProjectCreated = () => {
+      soundEngine.playTap();
+      window.dispatchEvent(new Event('storage_projects_updated'));
+    };
+
+    window.addEventListener('forja:next-action-done', handleNextAction);
+    window.addEventListener('forja:project-completed', handleProjectCompleted);
+    window.addEventListener('forja:project-advanced', handleProjectAdvanced);
+    window.addEventListener('forja:project-created', handleProjectCreated);
+
+    return () => {
+      window.removeEventListener('forja:next-action-done', handleNextAction);
+      window.removeEventListener('forja:project-completed', handleProjectCompleted);
+      window.removeEventListener('forja:project-advanced', handleProjectAdvanced);
+      window.removeEventListener('forja:project-created', handleProjectCreated);
+    };
+  }, []);
+
   return (
     <div className="forja-app -mx-4 -my-4 sm:-mx-6 sm:-my-6">
       <ForjaApp />
